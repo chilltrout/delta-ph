4d1bfa876afc8517c2e9c2b902d2031b2e293e50
chilltrout <rbrown92@me.com>
2025-03-31 23:40:45 +1000

    Create manifest.py

---
+++ manifest.py
@@ -0,0 +1,10 @@
+{
+  "domain": "ph_control",
+  "name": "pH Control",
+  "version": "1.0",
+  "documentation": "https://example.com/ph_control",
+  "dependencies": [],
+  "codeowners": ["@your_github_username"],
+  "requirements": [],
+  "config_flow": true
+}
