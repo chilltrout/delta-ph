34fe2beb6e47bd7c5ea0916686e65db71b8f2254
chilltrout <rbrown92@me.com>
2025-04-02 03:33:02 +1000

    Update const.py

--- custom_components/ph_control/const.py
+++ custom_components/ph_control/const.py
@@ -1,3 +1,9 @@
 """Constants for the pH Control integration."""
+
 DOMAIN = "ph_control"
+
+# Required attributes for amplitude and oscillation calculations
+ATTR_PEAKS = "peaks"
+ATTR_TROUGHS = "troughs"
 ATTR_AMPLITUDE = "amplitude"
+ATTR_OSCILLATIONS = "oscillations"
