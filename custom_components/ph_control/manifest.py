04afd19c0704b5ab09817f938cce95f75e4427b7
chilltrout <rbrown92@me.com>
2025-04-02 02:21:15 +1000

    Update manifest.py

--- custom_components/ph_control/manifest.py
+++ custom_components/ph_control/manifest.py
@@ -1,10 +1,10 @@
 {
   "domain": "ph_control",
   "name": "pH Control",
-  "version": "1.0",
-  "documentation": "https://example.com/ph_control",
-  "dependencies": [],
-  "codeowners": ["@your_github_username"],
-  "requirements": [],
-  "config_flow": true
+  "documentation": "https://github.com/chilltrout/delta-ph",
+  "dependencies": ["recorder"],
+  "codeowners": ["@chilltrout"],
+  "config_flow": true,
+  "version": "1.0.0",
+  "iot_class": "calculated"
 }
