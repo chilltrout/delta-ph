0b2565f9ee042a1d254749d3c2d50e7bb36db4af
chilltrout <rbrown92@me.com>
2025-04-02 03:02:04 +1000

    Update __init__.py

--- custom_components/ph_control/__init__.py
+++ custom_components/ph_control/__init__.py
@@ -24,6 +24,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
     
+    # Fix for the blocking import error
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     
     entry.async_on_unload(entry.add_update_listener(update_listener))
